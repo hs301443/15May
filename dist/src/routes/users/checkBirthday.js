dc88ed9b4f9a02a47947a5525d417b9ad0fa1784
hs301443 <106682004+hs301443@users.noreply.github.com>
2025-08-17 14:31:09 +0300

    birthday

---
+++ dist/src/routes/users/checkBirthday.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const catchAsync_1 = require("../../utils/catchAsync");
+const authenticated_1 = require("../../middlewares/authenticated");
+const checkBirthday_1 = require("../../controllers/users/checkBirthday");
+const router = (0, express_1.Router)();
+router.get("/", authenticated_1.authenticated, (0, catchAsync_1.catchAsync)(checkBirthday_1.isBirthdayToday));
---
+++ src/routes/users/checkBirthday.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import { catchAsync } from "../../utils/catchAsync";
+import { authenticated } from "../../middlewares/authenticated";
+import {isBirthdayToday } from "../../controllers/users/checkBirthday";
+const router = Router();
+
+router.get("/",authenticated,catchAsync(isBirthdayToday))
